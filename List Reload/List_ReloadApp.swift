64d8fc33d9ae4b844d3ac1df7a4e03b643fb5b9b
Duncan Babbage <duncan@avid.health>
2024-09-30 09:23:33 +1300

    Initial Commit

---
+++ List Reload/List_ReloadApp.swift
@@ -0,0 +1,20 @@
+//
+//  List_ReloadApp.swift
+//  List Reload
+//
+//  Created by Duncan Babbage on 30/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct List_ReloadApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
